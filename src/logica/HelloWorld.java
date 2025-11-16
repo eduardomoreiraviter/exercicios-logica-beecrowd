3fd4df757fa782a2518658ed1340f336dfa6a64a
Eduardo Moreira Viter <moreiraviter@outlook.com>
2025-11-15 22:22:43 -0300

    first commit

---
+++ src/logica/HelloWorld.java
@@ -0,0 +1,10 @@
+package logica;
+
+import java.io.IOException;
+
+public class HelloWorld {
+    public static void main(String[] args)  throws IOException{
+
+        System.out.print("Hello World!");
+    }
+}
